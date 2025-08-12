2bd32df4f57a9b03b623669d536bda2f395f1c32
muhammaaddsafii-dev <muhammaaddsafii@gmail.com>
2025-08-12 10:08:12 +0700

    revisi-npm-run-build-index-project

--- src/app/admin/dashboard/utilities/IndexProject/page.tsx
+++ src/app/admin/dashboard/utilities/IndexProject/page.tsx
@@ -1,9 +1,28 @@
-import GeometryViewer from "@/components/IndexProject/GeometryViewer";
+// import GeometryViewer from "@/components/IndexProject/GeometryViewer";
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <GeometryViewer />
+//     </main>
+//   );
+// }
+
+
+import dynamic from 'next/dynamic';
+
+const GeometryViewer = dynamic(
+  () => import('@/components/IndexProject/GeometryViewer'),
+  { 
+    ssr: false,
+    loading: () => <p>Loading...</p>
+  }
+);
 
 export default function Home() {
   return (
-    <main>
+    <div>
       <GeometryViewer />
-    </main>
+    </div>
   );
 }
