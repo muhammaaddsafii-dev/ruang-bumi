4aa37d26fe420126ae10a1f07382c299d60b89ee
muhammaaddsafii-dev <muhammaaddsafii@gmail.com>
2026-09-11 13:31:54 +0700

    feat: add public api base url

--- src/lib/apiConfig.ts
+++ src/lib/apiConfig.ts
@@ -1,2 +1,3 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
+  // process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://backend-ruangbumi-com-278881327745.asia-southeast1.run.app";
